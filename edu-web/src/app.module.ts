89c0555ac7840bb34692056da8d833165d6fd819
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-18 00:52:15 +0300

    feat(ui): add connection to do mongo db

--- edu-web/src/app.module.ts
+++ edu-web/src/app.module.ts
@@ -1,13 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AnnouncementsModule } from './announcements/announcements.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+      ConfigModule.forRoot({
+      isGlobal: true, // This makes env available everywhere
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    AnnouncementsModule;
+    AnnouncementsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
