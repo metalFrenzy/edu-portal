502c2709cfc569bbff26b9a53acb560023a87d75
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-17 21:01:57 +0300

    feat(ui): add the controller

--- edu-web/src/announcements/announcements.controller.ts
+++ edu-web/src/announcements/announcements.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { AnnouncementsService } from './announcements.service';
+import { CreateAnnouncementDto } from './dto/create-announcement.dto';
+import { UpdateAnnouncementDto } from './dto/update-announcement.dto';
 
 @Controller('announcements')
-export class AnnouncementsController {}
+export class AnnouncementsController {
+    constructor(private readonly service: AnnouncementsService) { }
+
+    @Post()
+    create(@Body() dto: CreateAnnouncementDto) {
+        return this.service.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.service.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.service.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateAnnouncementDto) {
+        return this.service.update(id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.service.remove(id);
+    }
+}
