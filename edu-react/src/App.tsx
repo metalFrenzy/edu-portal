3093ad365941f2b3d6f28dcecb323ce9d9d44564
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-18 23:07:55 +0300

    feat(ui): add redux to handle the login states

--- edu-react/src/App.tsx
+++ edu-react/src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RequireAuth from './routes/RequireAuth';
+import Dashboard from './pages/dashboard';
+import Home from './pages/home';
+
 function App() {
-  return <h1>Edu Portal Frontend</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
