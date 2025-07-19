fdaa130d13d86b0024100377c89fb2273c6a7729
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-19 22:25:42 +0300

    feat(ui): add styles

---
+++ edu-react/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+import './Sidebar.scss';
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>Edu Portal</h2>
+      <ul>
+        <li>Dashboard</li>
+        <li>Announcements</li>
+        <li>Quizzes</li>
+      </ul>
+    </aside>
+  );
+}
